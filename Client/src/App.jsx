feb36b12e1683c2b48cde66d9504432a8d152385
puvanakopis <puvanakopis@gmail.com>
2025-06-08 16:51:51 +0530

    add login page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,21 +4,25 @@ import { Toaster } from 'react-hot-toast'
 
 import Home from './Pages/Home'
 import Footer from './Componets/Footer'
+import { useAppContext } from './context/AppContext'
+import Login from './Componets/Login'
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes('/seller')
+  const {showUserLogin} =useAppContext()
+  
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
       <Toaster />
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
 
-      </div>        
-      {!isSellerPath && <Footer />}
+      </div>        {!isSellerPath && <Footer />}
 
     </div>
   )
